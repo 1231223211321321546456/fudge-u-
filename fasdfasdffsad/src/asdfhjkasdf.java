f0482c73c2e3c8ec07654ff29466afa267b8d902
Dominic <Dominic@Dominic-Tower>
2020-11-22 13:39:00 -0800

    new

--- fasdfasdffsad/src/asdfhjkasdf.java
+++ fasdfasdffsad/src/asdfhjkasdf.java
@@ -1,7 +1,6 @@
 
-public class HelloWorld {
+public class asdfhjkasdf {
 	public static void main(String[] args) {
 		System.out.println("Hello World");
-		System.out.println("fdsajkhlfsda");
 	}
 }
